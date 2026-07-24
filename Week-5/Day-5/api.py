a2482a20f958a232036e6c0069e93a27e245a1c6
Arslan <arslansohail2001@gmail.com>
2026-07-24 19:12:32 +0500

    Week 5 Day 5: Fix unresolved import warning in api.py

--- Week-5/Day-5/api.py
+++ Week-5/Day-5/api.py
@@ -7,6 +7,9 @@ import uuid
 import logging
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from triage_system import triage_app
 
 # --- Setup Logging ---
